415b458e818d0974e61a4732778df20a6a108fb9
Beatriz Roecker <152621606+beatrizrckr97@users.noreply.github.com>
2025-04-27 09:42:14 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -20,3 +20,12 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+]
