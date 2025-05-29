a74b491820e321a0b5e9e119590879e17023e9d9
Beatriz Roecker <152621606+beatrizrckr97@users.noreply.github.com>
2025-05-29 19:38:25 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'  # Para onde o usuário vai após o login
+LOGOUT_REDIRECT_URL = '/'  # Para onde o usuário vai após o logout
