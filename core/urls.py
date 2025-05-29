690cf37ba5656620e15d3d2affb3faf4f85f4eec
Beatriz Roecker <152621606+beatrizrckr97@users.noreply.github.com>
2025-05-29 19:37:10 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views  # Importa as views de autenticação do Django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('', include('core.solicitacoes.urls')),
+    path('', include('solicitacoes.urls')),  # Suas URLs do app solicitacoes
+    # URLs para login e logout
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
