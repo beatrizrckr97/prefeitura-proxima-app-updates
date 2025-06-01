b3dc9373dd8f0047891230fc1223acc524ca73ad
beatrizrckr97 <152621606+beatrizrckr97@users.noreply.github.com>
2025-06-01 17:14:52 -0300

    feat: atualiza models, admin e formulários do projeto integrador

---
+++ prefeitura-proxima-app-main/core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Servico, RequisicaoServico
+
+admin.site.register(Servico)
+admin.site.register(RequisicaoServico)
---
+++ prefeitura-proxima-app-main/core/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views  
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('dashboard/', views.home, name='home'),
+    path('novo-chamado/', views.novo_chamado, name='novo_chamado'),
+    path('ver-chamado/<int:chamado_id>/', views.ver_chamado, name='ver_chamado'),
+    path('editar-chamado/<int:chamado_id>/', views.editar_chamado, name='editar_chamado'),
+    path('excluir-chamado/<int:chamado_id>/', views.excluir_chamado, name='excluir_chamado'),
+]
